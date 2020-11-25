a68b6054413c01f5cfe2cd6c678b360bae83a12a
leonel <lf.naranjo11@uniandes.edu.co>
2020-11-25 13:42:08 -0500

    fix

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 sudo yum update
-sudo yum install docker.io
+sudo yum install -y yum-utils
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+    
+sudo yum install docker-ce docker-ce-cli containerd.io
 sudo usermod -aG docker $USER
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
