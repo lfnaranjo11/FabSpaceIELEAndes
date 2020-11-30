bcd63e7b2b4a0c89400a42b7f5fd2137554a60af
leonel <lf.naranjo11@uniandes.edu.co>
2020-11-30 13:39:50 -0500

    front env

--- FrontFabSpace/fabspaceuniandesfront/src/helpers/axios-requests.js
+++ FrontFabSpace/fabspaceuniandesfront/src/helpers/axios-requests.js
@@ -4,7 +4,7 @@ const instance = axios.create({
   // baseURL: 'http://LoadBalancerDespC-125555267.us-east-1.elb.amazonaws.com:8000/api/',
   //baseURL: 'http://LoadBalancerDespC-125555267.us-east-1.elb.amazonaws.com/api',
   //baseURL: 'https://jsonplaceholder.typicode.com/',
-  baseURL: 'http://127.0.0.1:8000/restapi/',
+  baseURL: `http://${process.env.REACT_APP_BACK_END}/restapi/`,
   responseType: 'json',
 });
 
